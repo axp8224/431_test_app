545956fc51c1c9a2eea6df72243ecbb28b57f0f0
aryan <aryan@darkaio.xyz>
2024-09-06 14:00:21 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :books
   root "books#index"
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
