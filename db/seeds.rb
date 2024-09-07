8a611b97797fa282252543f2fb66408a74cb9856
aryan <aryan@darkaio.xyz>
2024-09-07 15:22:50 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!([
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee" },
+  { title: "1984", author: "George Orwell" },
+  { title: "Pride and Prejudice", author: "Jane Austen" },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+])
